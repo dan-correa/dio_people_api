2094e4971a393f24705ef1214eb0ba6e536d9757
Daniel Alves <daniel.pesquisa.ic@gmail.com>
2021-06-28 23:48:22 -0300

    Developed list all method

--- src/main/java/one/digitalinnovation/personapi/controller/PersonalController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonalController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
@@ -30,5 +31,9 @@ public class PersonalController {
 
         return personService.createPerson(personDTO);
 
+    }
+@GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
     }
 }
